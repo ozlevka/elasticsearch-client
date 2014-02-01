642e9f07a8a693c5be6b6ec94336a0c46e39596d
levka <ozlevka@levka-pythondev.(none)>
2014-02-01 12:57:31 +0200

    Create basic operations

--- test/clienttest.js
+++ test/clienttest.js
@@ -4,10 +4,30 @@ debugger;
 var client = new elastic.ElasticClient();
 
 
-exports.testConnect = function()
+exports.testConnect = function(test)
 {
 	client.status(function(err, result)
 	{
-		
+		test.ok(result, 'result returned')
+		test.done();
 	});
 }
+
+exports.group = {
+	 createIndex : function(test) {
+	 	client.createIndex('hello', function(err, res){
+	 		test.ok(!err, 'Create index');
+	 		test.done();
+	 	});
+	 },
+
+	 deleteIndex : function(test) {
+	 	client.deleteIndex('hello', function(err, res){
+	 		test.ok(!err, 'delete index');
+	 		test.done();
+	 	});
+	 }
+
+};
+
+
