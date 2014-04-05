2632946f9cb2a4d27adec39e327750f86f67a691
unknown <levo@levo-7-lap.ankor.local>
2014-04-05 17:53:56 +0300

    Add free query function (runQuery)

--- commontest.js
+++ commontest.js
@@ -1,6 +1,7 @@
 var elastic = require('./lib/elasticclient');
 var qs = require('querystring');
 var request = require('request');
+var util = require('util');
 
 
 
@@ -17,6 +18,43 @@ function indexStatusTest() {
     });
 }
 
-indexStatusTest();
+
+function runQueryTest() {
+    client.runQuery(function(err, results){
+        if(err) console.error(err);
+        else console.log(results);
+    });
+}
+
+
+
+function runAggregateQuery() {
+    var query = {
+        "aggs": {
+            "range": {
+                "date_range": {
+                    "field": "created",
+                    "format": "dd-MM-yyyy H:mm",
+                    "ranges": [
+                        { "from" : "now-24H", "to": "now-20H" },
+                        { "from" : "now-20H", "to": "now-16H" },
+                        { "from" : "now-16H", "to": "now-12H" },
+                        { "from" : "now-12H", "to": "now-10H" },
+                        { "from" : "now-10H", "to": "now-8H" }
+                    ]
+                }
+            }
+        }
+    };
+
+    client.runQuery(query,function(err, results){
+        if(err) console.error(err);
+        else console.log(util.inspect(results));
+    });
+}
+
+runAggregateQuery();
+//runQueryTest();
+//indexStatusTest();
 
 
